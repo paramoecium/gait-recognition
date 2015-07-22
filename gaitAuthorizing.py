7e9c974f240c441b82faa8db19ddae9905360644
paramoecium <hank123456789321@yahoo.com.tw>
2015-07-23 00:59:42 +0800

    svm cross validate

--- gaitAuthorizing.py
+++ gaitAuthorizing.py
@@ -23,3 +23,11 @@ if __name__=='__main__':
 	for i, a in enumerate(RPDictionary.getAtoms()):
 		print i,a
 	writeFeature('./svm_data', RPDictionary.encoding(DataSet_1D), label)
+
+	Y_train = label
+	topicNum, X_train = svm_read_problem(args['topicFilePath'])
+	prob = svm_problem(Y_train, X_train)
+	param = svm_parameter('-v 5 -q')
+	model = svm_train(prob, param)
+	#print model
+	#svm_save_model('{}/svm_model'.format(args['outputDir']),model)
