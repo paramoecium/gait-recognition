4e449585aba2e053e7b8b94c87ab58e6c9ce009d
paramoecium <hank123456789321@yahoo.com.tw>
2015-08-18 14:25:50 +0800

    color adjustment

--- visualize.py
+++ visualize.py
@@ -16,14 +16,19 @@ def readArray(outputFilename):
 if __name__=='__main__':
     featureNum = 100
     X_train, Y_train = readFeature('./svm_train', featureNum)
+    colors = np.random.rand(len(Y_train))
     colors = Y_train
+    X_train = [X_train[i] for i in range(len(X_train)) if Y_train[i]!=3]
+    colors = [Y_train[i] for i in range(len(Y_train)) if Y_train[i]!=3]
+    colorMap = {0:'green',1:'red',2:'yellow'}
+    colors = [colorMap[c] for c in colors]
+    print colors
     X_iso = manifold.Isomap(n_neighbors=10, n_components=3).fit_transform(X_train)
-    print("Done.")
     '''
-    plt.scatter(X_iso[:,0], X_iso[:,1], c=colors, alpha=0.5)
+    plt.scatter(X_iso[:,0], X_iso[:,1], c=colors)
     plt.show()
     '''
     fig = plt.figure()
     ax = fig.add_subplot(111, projection='3d')
-    ax.scatter(X_iso[:,0], X_iso[:,1], X_iso[:,2], c=colors, marker='o',alpha=0.2)    
+    ax.scatter(X_iso[:,0], X_iso[:,1], X_iso[:,2], c=colors, marker='o')    
     plt.show()
